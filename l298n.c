248cfb6bd341b758df11c9f1766560c06fc846b9
cuki <mauricio@krebs.com.br>
2014-09-22 14:14:23 -0300

    pulso com ccp (compare)

--- l298n.c
+++ l298n.c
@@ -9,12 +9,42 @@
 
 #FUSES HS
 
-#use delay(clock=16MHz)
+#use delay(clock=15MHz)
 #use rs232(xmit=PIN_C6, baud=9600)
+
+#define ccp_nr 15000
+
+short ctrl;
+
+#int_ccp1
+void ccp1_isr() {
+	clear_interrupt(INT_CCP1);
+	ctrl = !ctrl;
+	if (ctrl) {
+		setup_ccp1(CCP_COMPARE_CLR_ON_MATCH);
+		set_timer1(0);
+		CCP_1 = ccp_nr;
+	} else {
+		setup_ccp1(CCP_COMPARE_SET_ON_MATCH);
+		set_timer1(0);
+		CCP_1 = ccp_nr;
+	}
+}
 
 int main(void) {
 
-	printf("hello");
-	return 0;
+	setup_ccp1(CCP_COMPARE_CLR_ON_MATCH);
+	set_timer1(0);
+	CCP_1 = ccp_nr;
+	setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
 
+	clear_interrupt(INT_CCP1);
+	enable_interrupts(INT_CCP1);
+	enable_interrupts(GLOBAL);
+
+	while (TRUE) {
+
+	}
+
+	return 0;
 }
--- makefile
+++ makefile
@@ -1,13 +1,10 @@
 CC=ccsc
 CFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT
 
-all: l298n compare
+all: l298n
 
 l298n: l298n.c
 	$(CC) $(CFLAGS) l298n.c
-
-compare: compare.c
-	$(CC) $(CFLAGS) compare.c
 
 clean:
 	rm *.cof *.err *.esym *.hex *.lst
