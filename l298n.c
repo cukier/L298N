4245ef5e3c82761970e8d77877299ad49b9b0e45
cuki <mauricio@krebs.com.br>
2014-09-23 11:14:52 -0300

    feito ontra digital trifasica com interrupcao

--- l298n.c
+++ l298n.c
@@ -10,93 +10,64 @@
 #FUSES HS
 
 #use delay(clock=15MHz)
+//#use delay(clock=9MHz)
 #use rs232(xmit=PIN_C6, baud=9600)
 
-#define ccp_nr 60000
-
 #define debouce 100
+#define tmr0reg 55118
+//#define tmr0reg 59285
 
 #define pin_sobe PIN_B0
 #define pin_desce PIN_B1
 
-short ctrl1;
-short ctrl2;
 short ctrl_bto = TRUE;
-short a;
-short b;
+short ba;
+short bb;
+short dir;
 
-#int_ccp1
-void ccp1_isr() {
-	clear_interrupt(INT_CCP1);
-	ctrl1 = !ctrl1;
-	if (ctrl1) {
-		setup_ccp1(CCP_COMPARE_CLR_ON_MATCH);
-		set_timer1(0);
-		CCP_1 = ccp_nr;
-	} else {
-		setup_ccp1(CCP_COMPARE_SET_ON_MATCH);
-		set_timer1(0);
-		CCP_1 = ccp_nr;
-	}
-}
+int estado;
+int saida[6] = { 0b101, 0b100, 0b110, 0b010, 0b011, 0b001 };
 
-#int_ccp2
-void ccp2_isr() {
-	clear_interrupt(INT_CCP2);
-	ctrl2 = !ctrl2;
-	if (ctrl2) {
-		setup_ccp2(CCP_COMPARE_CLR_ON_MATCH);
-		set_timer3(0);
-		CCP_2 = ccp_nr;
+#INT_TIMER0
+void isr_timer0() {
+	clear_interrupt(INT_TIMER0);
+	set_timer0(tmr0reg);
+	if (dir) {
+		estado++;
+		if (estado > 5)
+			estado = 0;
 	} else {
-		setup_ccp2(CCP_COMPARE_SET_ON_MATCH);
-		set_timer3(0);
-		CCP_2 = ccp_nr;
+		estado--;
+		if (estado > 5)
+			estado = 5;
 	}
+	output_c(saida[estado]);
 }
 
 int main(void) {
 
-	set_timer1(0);
-	setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
-	set_timer1(0);
-	setup_timer_3(T3_INTERNAL | T3_CCP2 | T3_DIV_BY_1);
-	set_timer3(0);
+	setup_timer_0(T0_INTERNAL | T0_DIV_1);
+	set_timer0(tmr0reg);
 
-	setup_ccp1(CCP_COMPARE_CLR_ON_MATCH);
-	CCP_1 = ccp_nr;
-	setup_ccp2(CCP_COMPARE_CLR_ON_MATCH);
-	CCP_2 = ccp_nr;
-
-	clear_interrupt(INT_CCP1);
-	clear_interrupt(INT_CCP2);
-//	enable_interrupts(INT_CCP1);
-//	enable_interrupts(INT_CCP2);
+//	enable_interrupts(INT_TIMER0);
 	enable_interrupts(GLOBAL);
 
 	while (TRUE) {
-		a = !input(pin_sobe);
-		b = !input(pin_desce);
+		ba = !input(pin_sobe);
+		bb = !input(pin_desce);
 
-		if (a ^ b) {
-			delay_ms(debouce);
+		if (ba ^ bb) {
 			if (ctrl_bto) {
 				ctrl_bto = FALSE;
-				if (a) {
-					clear_interrupt(INT_CCP1);
-					enable_interrupts(INT_CCP1);
-				} else {
-					clear_interrupt(INT_CCP2);
-					enable_interrupts(INT_CCP2);
-				}
+				delay_ms(debouce);
+				clear_interrupt(INT_TIMER0);
+				enable_interrupts(INT_TIMER0);
+				dir = ba;
 			}
 		} else if (!ctrl_bto) {
 			ctrl_bto = TRUE;
-			setup_ccp1(CCP_COMPARE_CLR_ON_MATCH);
-			setup_ccp2(CCP_COMPARE_CLR_ON_MATCH);
-			disable_interrupts(INT_CCP1);
-			disable_interrupts(INT_CCP2);
-			ctrl1 = ctrl2 = FALSE;
+			disable_interrupts(INT_TIMER0);
+			estado = 0;
 		}
 	}
 
